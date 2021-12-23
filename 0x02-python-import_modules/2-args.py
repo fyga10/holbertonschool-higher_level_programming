4ccfa01a749ac35772ba2dd968caaacf4627c5dd
fyga10 <frankgiraldo62@outlook.com>
2021-12-23 18:57:02 -0500

    t2a0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if length == 1:
         print("0 arguments.")
     elif length == 2:
-        print("1 argument: ")
+        print("1 argument:")
     else:
-        print("{} arguments: ".format(length - 1))
+        print("{} arguments:".format(length - 1))
     for i in range(1, length):
         print("{}: {}".format(i, argv[i]))
