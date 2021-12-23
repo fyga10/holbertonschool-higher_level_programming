a15b0474966314a389642568bcf04df2916548ce
fyga10 <frankgiraldo62@outlook.com>
2021-12-23 18:55:02 -0500

    t2a0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv)
     if length == 1:
-        print("0 arguments")
+        print("0 arguments.")
     elif length == 2:
         print("1 argument: ")
     else:
