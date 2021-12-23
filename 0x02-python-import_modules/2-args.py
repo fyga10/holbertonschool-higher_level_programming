0971e11651b61caac86fa8c2e42a2ef8bbeff712
fyga10 <frankgiraldo62@outlook.com>
2021-12-23 18:53:55 -0500

    t2a0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
         print("1 argument: ")
     else:
         print("{} arguments: ".format(length - 1))
-    for i in range (1,length):
+    for i in range(1, length):
         print("{}: {}".format(i, argv[i]))
-    
