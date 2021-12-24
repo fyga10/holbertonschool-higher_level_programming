9360e7a6509ee08f4f51c88c00727cb467499568
fyga10 <frankgiraldo62@outlook.com>
2021-12-23 19:19:43 -0500

    t3a0

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-length = len(argv)
-add = 0
-for i in range(1, length):
-    add += int(argv[i])
-print("{}".format(add))
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    add = 0
+    for i in range(1, length):
+        add += int(argv[i])
+    print("{}".format(add))
