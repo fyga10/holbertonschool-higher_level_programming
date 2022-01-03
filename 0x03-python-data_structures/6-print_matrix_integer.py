943049835e214a0a51ec4ba6e878c8b39303cb43
fyga10 <frankgiraldo62@outlook.com>
2022-01-03 15:47:39 -0500

    t6a0

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        for row in matrix:
+            a = 0
+            for column in row:
+                a += 1
+                if a == len (row):
+                    print("{:d}".format(column))
+                else:
+                    print("{:d}".format(column), end=" ")
+    if len(matrix[0]) == 0:
+        print("")
