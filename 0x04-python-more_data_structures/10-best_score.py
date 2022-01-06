787ba8c8e774fac27984fd137e68c4950ea1d44b
fyga10 <frankgiraldo62@outlook.com>
2022-01-05 19:18:56 -0500

    t10a0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        max_value = max(a_dictionary, key=a_dictionary.get)
-        return max_value
-    return None
+    max_ = max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    return max_
