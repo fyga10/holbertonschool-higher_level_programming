9e606f660e54117a4b3572f5459cf9d8e3f4c5ce
fyga10 <frankgiraldo62@outlook.com>
2022-01-17 18:12:45 -0500

    t7a0

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as Err:
-        print("Exception:", e, file=sys.stderr)
+        print("Exception:", Err, file=sys.stderr)
         return False
