9931b23e860941e0be8c34ac17d205189dcb3d8c
fyga10 <frankgiraldo62@outlook.com>
2022-01-18 18:37:11 -0500

    t2a0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square:
     """ Model of a square """
     def __init__(self, size):
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
