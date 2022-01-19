f5606585640a1b43c470ee980c487b167a9980c1
fyga10 <frankgiraldo62@outlook.com>
2022-01-18 19:01:16 -0500

    t4a0

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" definition of  a Square class"""
+
+
+class Square:
+    """ Model of a square """
+    def __init__(self, size=0):
+        """Initializer object"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size*self.__size
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
