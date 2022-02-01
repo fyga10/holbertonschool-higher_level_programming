337677136e44f1c0d2edcc84e2382569ec63e041
fyga10 <frankgiraldo62@outlook.com>
2022-02-01 17:38:47 -0500

    t0a1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""read_file function"""
 
-read_file = __import__('0-read_file').read_file
 
-read_file("my_file_0.txt")
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
