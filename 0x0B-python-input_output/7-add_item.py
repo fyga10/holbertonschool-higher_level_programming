02499299866d3b7ffbbe3d13dfadd7faa6a3f45f
fyga10 <frankgiraldo62@outlook.com>
2022-02-01 21:46:06 -0500

    t7a0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""adds arguments to list in json repr"""
+"""Write a script that adds all arguments to a Python list"""
 
 
 from sys import argv
-from pathlib import Path
-import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    with open('add_item.json', 'a', encoding='utf-8') as f:
-        if Path('add_item.json').stat().st_size == 0:
-            c_list = []
-        else:
-            c_list = load_from_json_file('add_item.json')
-        c_list.extend(argv[1:])
-        save_to_json_file(c_list, 'add_item.json')
+try:
+    lista = load_from_json_file("add_item.json") + argv[1:]
+    save_to_json_file(lista, "add_item.json")
+except Exception:
+    save_to_json_file(argv[1:], "add_item.json")
