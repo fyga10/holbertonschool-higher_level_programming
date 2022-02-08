be233c683582f2c81b47fd005e97525392bd0547
fyga10 <frankgiraldo62@outlook.com>
2022-02-07 21:16:20 -0500

    base and init

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""class Base"""
+
+import json
+
+
+class Base:
+    """constructor"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """set attribute"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """JSON string representation of
+        list_objs to a file"""
+        if list_objs is None or list_objs == []:
+            a = "[]"
+        else:
+            a = cls.to_json_string([o.to_dictionary() for o in list_objs])
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+            f.write(a)
