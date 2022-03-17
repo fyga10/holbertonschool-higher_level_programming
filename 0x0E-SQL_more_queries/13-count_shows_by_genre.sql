01f429d9df420eecb950c1086d243b5c1221a12f
fyga10 <frankgiraldo62@outlook.com>
2022-03-17 18:06:52 -0500

    t13a0

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List all the genres of shows and the count of each one.
+SELECT gr.name AS genre,
+       COUNT(shgr.genre_id) AS number_of_shows FROM tv_genres AS gr
+       JOIN tv_show_genres AS shgr
+       ON gr.id=shgr.genre_id
+       GROUP BY shgr.genre_id
+       ORDER BY number_of_shows DESC;
