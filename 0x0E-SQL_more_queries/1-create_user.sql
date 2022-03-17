5df4eaf1b266a3c09fbb925fae8013fd7df4d750
fyga10 <frankgiraldo62@outlook.com>
2022-03-17 11:37:26 -0500

    t1a0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Create an user.
+-- Creates the MySQL server user user_0d_1
+-- Query to perform operation
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
