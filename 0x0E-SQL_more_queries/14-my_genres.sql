abeced761ed72db0d6400422157e7cf4917abac9
fyga10 <frankgiraldo62@outlook.com>
2022-03-17 18:09:09 -0500

    t14a0

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- List all the genres of Dexter show.
+SELECT gr.name name FROM tv_genres AS gr
+       JOIN tv_show_genres AS shgr
+       ON gr.id=shgr.genre_id
+       JOIN tv_shows AS sh
+       ON shgr.show_id=sh.id
+       WHERE sh.title = "Dexter"
+       ORDER BY gr.name ASC;
