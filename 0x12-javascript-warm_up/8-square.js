360b39211b5e9404215f644adad8aaa7e2ced861
fyga10 <frankgiraldo62@outlook.com>
2022-04-04 16:26:26 -0500

    t8a0

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) === false) {
   for (let i = 0; i < process.argv[2]; i++) {
-      console.log('X'.repeat(process.argv[2]));
+    console.log('X'.repeat(process.argv[2]));
   }
 } else {
   console.log('Missing size');
