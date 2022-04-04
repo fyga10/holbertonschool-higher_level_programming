a8fac32a0919c69ae2762ee76d2085351d6ec2f3
fyga10 <frankgiraldo62@outlook.com>
2022-04-04 14:44:05 -0500

    t7a0

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let n = 0;
+if (isNaN(process.argv[2]) === false) {
+  let num = process.argv[2];
+  while (n < num) {
+    console.log('C is fun');
+    n++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
