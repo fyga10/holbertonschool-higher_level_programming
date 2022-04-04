c7f41dfef4f0f4a6f3e32d8989697bcab5f7b270
fyga10 <frankgiraldo62@outlook.com>
2022-04-04 18:48:57 -0500

    t11a1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const sorted = process.argv.slice(2).sort();
-  console.log(sorted[sorted.length - 2]);
+  console.log(`${sorted[sorted.length - 2]}`);
 }
