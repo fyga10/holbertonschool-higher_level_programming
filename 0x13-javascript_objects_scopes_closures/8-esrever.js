271766aae58359adbd94853f8cefcc5208367c0d
fyga10 <frankgiraldo62@outlook.com>
2022-04-05 18:16:59 -0500

    t8a0

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-exports.esrever = function (list){
+exports.esrever = function (list) {
   const le = list.length - 1;
-  let revers = [];
-  for (i = le; i >= 0; i--){
+  const revers = [];
+  for (i = le; i >= 0; i--) {
     revers.push(list[i]);
   }
   return revers;
-}
+};
