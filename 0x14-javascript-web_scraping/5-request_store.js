e7b2464db0d3520dd172eea22a304a04e199955a
fyga10 <frankgiraldo62@outlook.com>
2022-04-12 09:47:14 -0500

    t5a0

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const filepath = process.argv[3];
+request(process.argv[2], (err, response, body) =>{
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFileSync(filepath, response.body);
+});
