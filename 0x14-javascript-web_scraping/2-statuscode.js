b3a82edca066bf3106f0ab9097a5300bb43c634b
fyga10 <frankgiraldo62@outlook.com>
2022-04-12 08:31:48 -0500

    t2a0

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ request(process.argv[2], (err, response) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });
