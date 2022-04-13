79b47aea6317d22d9b19ecbd2e5a294950668bf3
fyga10 <frankgiraldo62@outlook.com>
2022-04-13 08:37:59 -0500

    t5a1

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 const filepath = process.argv[3];
-request(process.argv[2], (err, response, body) =>{
+request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   }
